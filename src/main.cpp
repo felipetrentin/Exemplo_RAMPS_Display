a99c926e685540ebd77eb0f478698ba6fd31fa91
felipetrentin <felipetrentintecnico@hotmail.com>
2024-03-06 22:50:03 -0300

    encoders test

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,91 @@
 
 U8GLIB_ST7920_128X64_4X tela(LCD_PINS_SCK, LCD_PINS_MOSI, LCD_PINS_CS);
 
-void setup(){
+void setup()
+{
   tela.begin();
   Serial.begin(9600);
+  // tela:
   pinMode(BEEPER_PIN, OUTPUT);
   pinMode(BTN_ENC, INPUT_PULLUP);
-  pinMode(MOSFET_A_PIN, OUTPUT);
-  pinMode(MOSFET_C_PIN, OUTPUT);
   pinMode(KILL_BTN_PIN, INPUT_PULLUP);
-  digitalWrite(MOSFET_A_PIN, LOW);
-  digitalWrite(MOSFET_C_PIN, LOW);
+
+  // sensor
   pinMode(X_MIN_PIN, INPUT);
   pinMode(X_MAX_PIN, INPUT_PULLUP);
-  tone(BEEPER_PIN, 440, 50);
-  delay(50);
-  tone(BEEPER_PIN, 440*2, 50);
-  delay(50);
-  tone(BEEPER_PIN, 440*3, 50);
+
+  // atuador
+  pinMode(MOSFET_A_PIN, OUTPUT);
+  pinMode(MOSFET_C_PIN, OUTPUT);
+  digitalWrite(MOSFET_A_PIN, LOW);
+  digitalWrite(MOSFET_C_PIN, LOW);
+
+  pinMode(E0_ENABLE_PIN, OUTPUT);
+  pinMode(E0_DIR_PIN, OUTPUT);
+  pinMode(E0_STEP_PIN, OUTPUT);
+
+  digitalWrite(BEEPER_PIN, HIGH);
+  delay(100);
+  digitalWrite(BEEPER_PIN, LOW);
+
+  cli();
+  TCCR4A = 0;
+  TCCR4B = 0;
+  TCNT4 = 0;
+
+  OCR4A = 15624;
+
+  TCCR4B |= (1 << WGM12);
+  TCCR4B |= (1 << CS12) | (1 << CS10);
+  TIMSK4 |= (1 << OCIE4A);
+  sei();
+
+  digitalWrite(E0_ENABLE_PIN, LOW);
+}
+
+bool toggle = false;
+ISR(TIMER4_COMPA_vect)
+{
+  digitalWrite(E0_STEP_PIN, toggle);
+  toggle = !toggle;
+}
+
+unsigned long last_run = 0;
+int counter = 0;
+
+void shaft_moved()
+{
+  if (millis() - last_run > 5)
+  {
+    if (digitalRead(4) == 1)
+    {
+      if (counter < 9)
+        counter++;
+    }
+    if (digitalRead(4) == 0)
+    {
+      if (counter > 0)
+        counter--;
+    }
+    last_run = millis();
+  }
 }
 
 // big font u8g_font_10x20
-void loop(){
+void loop()
+{
   tela.firstPage();
-  do {
+  do
+  {
     tela.setFont(u8g_font_courB14r);
-    tela.drawStr(0,20,"Bandeirnhas");
-    tela.drawStr(0,44,digitalRead(X_MAX_PIN)  ? "off" : "on ");
-    tela.drawStr(50,44,digitalRead(X_MIN_PIN) ? "off" : "on ");
-    if(!digitalRead(BTN_ENC)){
+    tela.drawStr(0, 20, "Bandeirnhas");
+    tela.drawStr(0, 44, digitalRead(X_MAX_PIN) ? "off" : "on ");
+    tela.drawStr(50, 44, digitalRead(X_MIN_PIN) ? "off" : "on ");
+    if (!digitalRead(BTN_ENC))
+    {
       tela.drawCircle(100, 50, 5);
     }
-  } while ( tela.nextPage() );
+  } while (tela.nextPage());
   digitalWrite(MOSFET_C_PIN, !digitalRead(BTN_ENC));
   digitalWrite(MOSFET_A_PIN, !digitalRead(KILL_BTN_PIN));
 }
